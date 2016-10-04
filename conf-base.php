e59dc531564df97bdee8bc961675d33e466ffb29
nalomysouza <nalomysouza@gmail.com>
2016-10-04 13:42:46 -0300

    Mudar títulos e descrições do campo 4. Agentes

---
+++ conf-base.php
@@ -0,0 +1,29 @@
+<?php
+return array(
+    'registration.ownerDefinition' => array(
+        'required' => true,
+        'label' => 'Agente responsável pelo Projeto',
+        'agentRelationGroupName' => 'owner',
+        'description' => 'Indicar pessoa física que ficará responsável pela execução do Projeto apresentado para este edital',
+        'type' => 1,
+        'requiredProperties' => array('documento', 'raca', 'dataDeNascimento', 'genero', 'emailPrivado', 'telefone1')
+    ),
+    'registration.agentRelations' => array(
+        array(
+            'required' => false,
+            'label' => 'Instituição Proponente',
+            'agentRelationGroupName' => 'instituicao',
+            'description' => 'Indicar pessoa jurídica proponente do Projeto apresentado para este edital',
+            'type' => 2,
+            'requiredProperties' => array('documento', 'dataDeNascimento', 'emailPrivado', 'telefone1')
+        ),
+        array(
+            'required' => false,
+            'label' => 'Coletivo',
+            'agentRelationGroupName' => 'coletivo',
+            'description' => 'Agente coletivo sem CNPJ, com os campos Data de Nascimento/Fundação e Email Privado obrigatoriamente preenchidos',
+            'type' => 2,
+            'requiredProperties' => array('dataDeNascimento', 'emailPrivado')
+        )
+    )
+);
