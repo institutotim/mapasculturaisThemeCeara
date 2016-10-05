3589c78960dcce7157b4ebafc8a3f02b70dc171a
nalomysouza <nalomysouza@gmail.com>
2016-10-04 21:59:22 -0300

    Adicionar Selo 50 anos e Logo Siscult no Mapa Cultural do Ceará

--- layouts/parts/header-logo.php
+++ layouts/parts/header-logo.php
@@ -1,3 +1,4 @@
 <h1 id="brand-logo">
-    <a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-ceara.png'); ?>" /></a>
+    <a style="float: left; padding-top: 6px;" href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-ceara.png'); ?>"/></a>
+    <a style="float: right; padding-left: 3px;" href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-siscult.png'); ?>"/></a>
 </h1>
